0833b962348d8b449dc97d7fa60c244d02e783d6
sanya309 <69692164+sanya309@users.noreply.github.com>
2021-08-15 01:40:21 -0700

    Create function.js

---
+++ function.js
@@ -0,0 +1,24 @@
+<script type="text/javascript" src="https://code.jquery.com/jquery-1.8.2.js"></script>
+
+<script type='text/javascript'>
+$(function(){
+var overlay = $('<div id="overlay"></div>');
+overlay.show();
+overlay.appendTo(document.body);
+$('.popup').show();
+$('.close').click(function(){
+$('.popup').hide();
+overlay.appendTo(document.body).remove();
+return false;
+});
+
+
+ 
+
+$('.x').click(function(){
+$('.popup').hide();
+overlay.appendTo(document.body).remove();
+return false;
+});
+});
+</script>
